5675be08982f1eeb1ca3f5291cd1db423324f7be
David McClure <davidwilliammcclure@gmail.com>
2015-12-07 11:54:46 -0800

    Implement SuffixTree#_indexOffsets().

--- src/suffix-tree.js
+++ src/suffix-tree.js
@@ -1,16 +1,43 @@
 
 
+import _ from 'lodash';
+
+
 export default class {
 
 
   /**
    * Initialize the tree.
    *
-   * @param {Array} seq - A sequence of tokens.
+   * @param {Array<String>} seq - A sequence of tokens.
    */
   constructor(seq) {
     this.seq = seq;
-    // TODO
+    this._indexOffsets();
+  }
+
+
+  /**
+   * Map token -> [offsets].
+   */
+  _indexOffsets() {
+
+    this.offsets = {};
+
+    _.each(this.seq, (token, i) => {
+
+      // Initialize the offset array, if it doesn't exist.
+      if (!_.has(this.offsets, token)) {
+        this.offsets[token] = [i];
+      }
+
+      // Otherwise, register the new offset.
+      else {
+        this.offsets[token].push(i);
+      }
+
+    });
+
   }
 
 
