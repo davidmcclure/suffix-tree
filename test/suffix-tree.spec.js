9e7df1e806490ec37b701a9f4387971bcdd6cf85
David McClure <davidwilliammcclure@gmail.com>
2015-12-07 11:36:50 -0800

    Add .editorconfig.

---
+++ test/suffix-tree.spec.js
@@ -0,0 +1,19 @@
+
+
+import { assert } from 'chai';
+import SuffixTree from '../src/suffix-tree';
+
+
+describe('SuffixTree', function() {
+
+  describe('#query', function() {
+
+    it('generates a suffix tree from a root token', function() {
+
+      let tree = new SuffixTree(['A', 'B', 'C']);
+
+    });
+
+  });
+
+});
