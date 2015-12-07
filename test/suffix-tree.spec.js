da8a506c3c227984ad363884781efd76b0539d6f
David McClure <davidwilliammcclure@gmail.com>
2015-12-07 11:46:46 -0800

    Stub in basic failing test for subtree querying.

--- test/suffix-tree.spec.js
+++ test/suffix-tree.spec.js
@@ -1,6 +1,6 @@
 
 
-import { assert } from 'chai';
+import { expect } from 'chai';
 import SuffixTree from '../src/suffix-tree';
 
 
@@ -10,7 +10,48 @@ describe('SuffixTree', function() {
 
     it('generates a suffix tree from a root token', function() {
 
-      let tree = new SuffixTree(['A', 'B', 'C']);
+      let tree = new SuffixTree([
+        'A', '1', '2',
+        'A', '3', '4',
+        'A', '5', '6',
+      ]);
+
+      expect(tree.query('A')).to.equal({
+        name: 'A',
+        size: 1,
+        children: [
+          {
+            name: '1',
+            size: 1,
+            children: [
+              {
+                name: '2',
+                size: 1
+              }
+            ]
+          },
+          {
+            name: '3',
+            size: 1,
+            children: [
+              {
+                name: '4',
+                size: 1
+              }
+            ]
+          },
+          {
+            name: '5',
+            size: 1,
+            children: [
+              {
+                name: '6',
+                size: 1
+              }
+            ]
+          },
+        ]
+      });
 
     });
 
